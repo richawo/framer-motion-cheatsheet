9f10c1f4a6a2f071f433ec059b88f55dd9c3add2
Richard Awoyemi <35015261+RichardAwoyemi@users.noreply.github.com>
2023-09-18 00:39:59 +0100

    Implemented back button functionality to navigate back to previous screen.

--- src/components/card-header.tsx
+++ src/components/card-header.tsx
@@ -1,15 +1,24 @@
 "use client";
 
+import { usePathname } from "next/navigation";
+import BackIcon from "./back-icon";
+
 type props = {
   title: string;
   subtitle: string;
 };
 
 export default function CardHeader({ title, subtitle }: props) {
+  const pathname = usePathname();
   return (
-    <div className="bg-[#0a0a0a]/80 min-h-[3rem] py-3 px-6 items-center text-left w-full h-min flex flex-col gap-1 border-b border-[#191919] text-xs">
-      <p className="mr-auto">{title}</p>
-      <p className="mr-auto text-white/50">{subtitle}</p>
+    <div className="bg-[#0a0a0a]/80 min-h-[3rem] items-center text-left w-full h-min flex border-b border-[#191919] text-xs">
+      {pathname.length > 1 && <div className="px-3 aspect-square min-w-[3rem] border-r border-[#191919] h-full flex items-center justify-center">
+        <BackIcon className="h-5 w-5" />
+      </div>}
+      <div className="flex flex-col py-3 px-6 gap-1">
+        <p className="mr-auto">{title}</p>
+        <p className="mr-auto text-white/50">{subtitle}</p>
+      </div>
     </div>
   );
 }
